8fa145c4a466757560b4835192e4e13041c0205b
Gaurav Saini <sainigaurav.2009@gmail.com>
2020-03-27 20:38:18 +0530

    Added filter by tags on blog posts

--- src/components/Blog components/BlogTitle.vue
+++ src/components/Blog components/BlogTitle.vue
@@ -2,6 +2,9 @@
   <div class="blog__post">
     <h1 v-html="getPost.title" v-if="getPost.title"></h1>
     <h1 v-html="previewContent" v-else></h1>
+    <div class="blog__post-tags" v-if="getPost.tags && getPost.tags.length">
+      <span v-for="(tag,idx) in getPost.tags" :key="idx">{{tag}}</span>
+    </div>
   </div>
 </template>
 
@@ -46,6 +49,23 @@ export default {
   color: rgb(255, 161, 38);
   font-size: 3rem;
   margin: 0.8rem;
+  margin-bottom: 0.3rem;
   text-align: center;
+}
+
+.blog__post-tags {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
+  margin: 0;
+}
+.blog__post-tags > span {
+  margin: 0.5rem;
+  padding: 0.5rem;
+  background: rgb(255, 161, 38);
+  color: #2e3342;
+  border-radius: 0.2rem;
 }
 </style>
--- src/components/Blog components/Blogs.vue
+++ src/components/Blog components/Blogs.vue
@@ -1,5 +1,13 @@
 <template>
   <main>
+    <div>
+      <b>Top tags:</b>&nbsp;&nbsp;&nbsp;
+      <span v-for="(tag,idx) in getTagsByCount()" 
+        :key="idx"
+        :value="tag[1].toLowerCase()"
+        @click="toggleTagInData($event)">{{tag[1]}}
+      </span>
+    </div>
     <blog-post v-for="(blogPost, idx) in blogPosts" :key="idx" :blogPost="blogPost"></blog-post>
   </main>
 </template>
@@ -8,12 +16,59 @@
 import blogPost from "./Blog.vue";
 
 export default {
+  data() {
+    return {
+      tags: []
+    }
+  },
   components: {
     blogPost
   },
+  methods: {
+    toggleTagInData(event){
+      let el = event.target;
+      let tag = el.getAttribute("value");
+      let idx = this.tags.indexOf(tag);
+      if (idx == -1) {
+        this.tags.push(tag);
+        el.classList.add("active-tag");
+      } else {
+        this.tags.splice(idx, 1);
+        el.className = "";
+        el.classList.remove("active-tag");
+      }
+    },
+    getTagsByCount() {
+      let tags = {}
+      for(let post of this.$store.getters.blogPosts) {
+        for(let tag of post.tags) {
+          if(tag in tags) {
+            tags[tag]++;
+          } else {
+            tags[tag] = 1;
+          }
+        }
+      }
+      let res = [];
+      for(let tag in tags) {
+        res.push([tags[tag], tag]);
+      }
+      return res.sort((el1, el2) => parseInt(el1[0]) < parseInt(el2[0])).slice(0, 10);
+    }
+  },
   computed: {
     blogPosts() {
-      return this.$store.getters.blogPosts;
+      if (this.tags.length == 0) {
+        return this.$store.getters.blogPosts;
+      }
+      return this.$store.getters.blogPosts.filter(el => {
+        for(let tag of el.tags) {
+          if (this.tags.includes(tag.toLowerCase())) {
+            return true;
+          }
+        }
+        return false;
+      });
     }
   },
   beforeMount() {
@@ -21,7 +76,7 @@ export default {
 		if (res.status == 200) this.$store.dispatch("setPosts", res.body)
 		else throw "Wordpress call failed"
 	}).catch(err => this.$store.dispatch("initPosts"));
-  }
+  },
 };
 </script>
 <style>
@@ -29,5 +84,30 @@ main {
   max-width: 70rem;
   margin: auto;
   margin-top: 5rem;
+}
+main > div:first-of-type {
+  position: relative;
+  top: -1.1rem;
+  padding: 1.25rem;
+  padding-left: 2rem;
+  margin: 1rem;
+  border-radius: 0 0 0.3rem 0.3rem;
+  box-shadow: 0px 0px 7px 4px rgba(0, 0, 0, 0.15);
+}
+
+main > div:first-of-type span {
+  margin: 0.5rem;
+  padding: 0.5rem;
+  background: rgb(255, 161, 38);
+  color: #2e3342;
+  border-radius: 0.2rem;
+  cursor: pointer;
+  transition: background,color 0.2s linear;
+}
+
+.active-tag {
+  background: rgb(204, 128, 28) !important;
+  color: white !important;
+  box-shadow: 0 0 0.4rem rgba(25, 25, 25, 0.30);
 }
 </style>
