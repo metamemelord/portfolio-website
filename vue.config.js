bbfbc75d69c8668b4406119bf5ca59c990f5c676
Gaurav Saini <sainigaurav.2009@gmail.com>
2020-06-24 02:01:52 +0530

    Added webpack config

---
+++ vue.config.js
@@ -0,0 +1,32 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        chunks: 'async',
+        maxSize: 200000,
+        minChunks: 1,
+        maxAsyncRequests: 6,
+        maxInitialRequests: 4,
+        automaticNameDelimiter: '~',
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true
+          }
+        }
+      },
+      minimizer: [
+        new UglifyJsPlugin({
+          parallel: 4,
+        })
+      ],
+    },
+  }
+};
