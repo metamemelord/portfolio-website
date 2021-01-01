3e33b0361f453f7c47099a955d795f87b26edd61
Gaurav Saini <sainigaurav.2009@gmail.com>
2021-01-02 02:44:57 +0530

    Added foramtting

--- model/metadata_item_type.go
+++ model/metadata_item_type.go
@@ -1,7 +1,8 @@
-package model 
+package model
 
 // go:generate enumer -trimprefix=MetadataItemType -type=MetadataItemType -transform=snake -json -sql
 type MetadataItemType int
+
 const (
 	MetadataItemTypeNone MetadataItemType = iota
 	MetadataItemTypeQueryParameter
--- pkg/core/constants.go
+++ pkg/core/constants.go
@@ -1,10 +1,10 @@
 package core
 
 const (
-	AMPERSAND    = "&"
+	AMPERSAND     = "&"
 	QUESTION_MARK = "?"
-	EMPTY_STRING = ""
-	DATE_FORMAT  = "2006-01-02 15:04:05"
-	ROUTING_KEY  = "routing_key"
-	PATH_LABEL   = "path"
+	EMPTY_STRING  = ""
+	DATE_FORMAT   = "2006-01-02 15:04:05"
+	ROUTING_KEY   = "routing_key"
+	PATH_LABEL    = "path"
 )
