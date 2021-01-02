ada3b06385e79818bbef11a253c1d641414e6899
Gaurav Saini <sainigaurav.2009@gmail.com>
2021-01-02 11:46:56 +0530

    Fixed a bug with json deserialization

--- model/metadata_item_type.go
+++ model/metadata_item_type.go
@@ -1,9 +1,9 @@
 package model
 
-// go:generate enumer -trimprefix=MetadataItemType -type=MetadataItemType -transform=snake -json -sql
+//go:generate enumer -trimprefix=MetadataItemType -type=MetadataItemType -transform=snake -json -sql
 type MetadataItemType int
 
 const (
 	MetadataItemTypeNone MetadataItemType = iota
-	MetadataItemTypeQueryParameter
+	MetadataItemTypeQueryParam
 )
