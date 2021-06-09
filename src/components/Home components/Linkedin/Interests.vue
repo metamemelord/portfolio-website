3de4db97eefabcc05265ccc616239d755c065c02
Gaurav Saini <gaurav.s.saini@oracle.com>
2021-06-09 16:45:51 +0530

    Modified Interests section

--- src/components/Home components/Linkedin/Interests.vue
+++ src/components/Home components/Linkedin/Interests.vue
@@ -2,32 +2,32 @@
   <div class="linkedin__interests">
     <h1 style="margin: 0">Interests</h1>
     <p>
-      <b>Top interests:</b> Software Engineering, Full-stack Development, Quantum Information System, Deep Learning, Genetic Algorithms.
+      <b>Top interests:</b> Software Engineering, Distributed Systems, Serverless, Full-stack Development, Quantum Information System, Deep Learning, Genetic Algorithms.
       <br>
       <br>
-      <b>Domain:</b> Web-development, eCommerce, Quantum Computing, Data Science.
+      <b>Domain:</b> Serverless, Cloud Infrastructure, Web-development, eCommerce, Quantum Computing.
       <br>
       <br>
-      <b>Server Side Technologies:</b> Spring, Node.js, Express.js, Django.
+      <b>Server Side Technologies:</b> Hyper (Rust), Gin (Go), Node.js, Express.js, Django.
       <br>
       <br>
-      <b>Client Side Technologies:</b> Vue.js, Qt.
+      <b>Client Side Technologies:</b> Vue.js, Nuxt.js, Qt.
       <br>
       <br>
       <b>Genral Purpose Programing and Scripting languages:</b>
-      C++, Java, Python, JavaScript
+      Rust, Go, C#, C++, Java, Python, JavaScript
       <br>
       <br>
-      <b>Databases:</b> MySQL, MongoDB (including ORM Frameworks like Mongoose), Neo4j.
+      <b>Databases:</b> Postgres, MySQL, MongoDB (including ORM Frameworks like Mongoose), Neo4j.
       <br>
       <br>
-      <b>Build, CI/CD, Code Review, and Testing Tools:</b> Ant, Maven, Gradle, CMake, NPM, Docker, Jenkins, JMeter, Loadtest (npm), Postman, SonarQube.
+      <b>Build, CI/CD, Code Review, and Testing Tools:</b> CircleCI, TravisCI, Cargo, CMake, NPM, Docker, Jenkins, JMeter, Loadtest (npm), Postman, SonarQube.
       <br>
       <br>
-      <b>Source code management:</b> Github.
+      <b>Source code management:</b> Github, Bitbucket.
       <br>
       <br>
-      <b>What I do in free time:</b> Simulating Quantum Algorithms, Learn about NNs and Genetic Algorithm, Read about graph databases, Do competitive coding, explore data structures, improving my understanding of Design Patterns and problem solving, and read CPP Reference.
+      <b>What I do in free time:</b> Diving deep into AsyncIO, Learning about Hypervisors, Simulating Quantum Algorithms, Learn about NNs and Genetic Algorithm, Read about graph databases, Do competitive coding, explore data structures, improving my understanding of Design Patterns and problem solving, and read CPP Reference.
       <br>
       <br>
       <b>Strengths:</b> Problem solving, Zeal to learn, Having fruitful technical conversations.
